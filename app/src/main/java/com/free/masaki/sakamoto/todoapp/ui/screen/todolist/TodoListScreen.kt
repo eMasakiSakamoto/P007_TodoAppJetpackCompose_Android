18e98a25454dd0e5ba3a802d088c0d7449c2c8de
坂本将紀 <sbl.sakamoto.masaki@gmail.com>
2026-05-09 16:54:13 +0900

    Todo追加ボタンの追加。

--- app/src/main/java/com/free/masaki/sakamoto/todoapp/ui/screen/todolist/TodoListScreen.kt
+++ app/src/main/java/com/free/masaki/sakamoto/todoapp/ui/screen/todolist/TodoListScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -46,6 +47,11 @@ fun TodoListScreen(
                     }
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddClick) {
+                Text("+")
+            }
         }
     ) { padding ->
         Column(
