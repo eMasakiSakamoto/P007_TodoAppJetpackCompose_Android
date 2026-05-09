c90d90be2a934ac58f183182086e6eb4caa7f055
坂本将紀 <sbl.sakamoto.masaki@gmail.com>
2026-05-09 15:25:00 +0900

    各UseCaseの追加。

--- app/src/main/java/com/free/masaki/sakamoto/todoapp/domain/usecase/MoveTodoToTrashUseCase.kt
+++ app/src/main/java/com/free/masaki/sakamoto/todoapp/domain/usecase/MoveTodoToTrashUseCase.kt
@@ -1,2 +1,12 @@
 package com.free.masaki.sakamoto.todoapp.domain.usecase
 
+import com.free.masaki.sakamoto.todoapp.data.repository.TodoRepository
+import javax.inject.Inject
+
+class MoveTodoToTrashUseCase @Inject constructor(
+    private val repository: TodoRepository,
+) {
+    suspend operator fun invoke(todoId: Long) {
+        repository.moveToTrash(todoId)
+    }
+}
--- app/src/main/java/com/free/masaki/sakamoto/todoapp/domain/usecase/RestoreTodoUseCase.kt
+++ app/src/main/java/com/free/masaki/sakamoto/todoapp/domain/usecase/RestoreTodoUseCase.kt
@@ -1,2 +1,12 @@
 package com.free.masaki.sakamoto.todoapp.domain.usecase
 
+import com.free.masaki.sakamoto.todoapp.data.repository.TodoRepository
+import javax.inject.Inject
+
+class RestoreTodoUseCase @Inject constructor(
+    private val repository: TodoRepository,
+) {
+    suspend operator fun invoke(todoId: Long) {
+        repository.restoreTodo(todoId)
+    }
+}
