48a2a5796a1d74e5c84a56fb9cd66edf0893114a
坂本将紀 <sbl.sakamoto.masaki@gmail.com>
2026-05-09 15:00:12 +0900

    TodoRepositoryの追加。

---
+++ app/src/main/java/com/free/masaki/sakamoto/todoapp/data/repository/TodoRepository.kt
@@ -0,0 +1,16 @@
+package com.free.masaki.sakamoto.todoapp.data.repository
+
+import com.free.masaki.sakamoto.todoapp.domain.model.Todo
+import kotlinx.coroutines.flow.Flow
+
+interface TodoRepository {
+    fun observeActiveTodos(): Flow<List<Todo>>
+    fun observeTrashedTodos(): Flow<List<Todo>>
+
+    suspend fun getTodoById(id: Long): Todo?
+    suspend fun addTodo(todo: Todo): Long
+    suspend fun updateTodo(todo: Todo)
+    suspend fun moveToTrash(todoId: Long)
+    suspend fun restoreTodo(todoId: Long)
+    suspend fun deleteTodoPermanently(todoId: Long)
+}
---
+++ app/src/main/java/com/free/masaki/sakamoto/todoapp/data/repository/TodoRepositoryImpl.kt
@@ -0,0 +1,48 @@
+package com.free.masaki.sakamoto.todoapp.data.repository
+
+import com.free.masaki.sakamoto.todoapp.data.local.TodoDao
+import com.free.masaki.sakamoto.todoapp.data.mapper.toDomain
+import com.free.masaki.sakamoto.todoapp.data.mapper.toEntity
+import com.free.masaki.sakamoto.todoapp.domain.model.Todo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class TodoRepositoryImpl @Inject constructor(
+    private val todoDao: TodoDao
+) : TodoRepository {
+
+    override fun observeActiveTodos(): Flow<List<Todo>> {
+        return todoDao.observeActiveTodos().map { list -> list.map { it.toDomain() } }
+    }
+
+    override fun observeTrashedTodos(): Flow<List<Todo>> {
+        return todoDao.observeTrashedTodos().map { list -> list.map { it.toDomain() } }
+    }
+
+    override suspend fun getTodoById(id: Long): Todo? {
+        return todoDao.getTodoById(id)?.toDomain()
+    }
+
+    override suspend fun addTodo(todo: Todo): Long {
+        return todoDao.insert(todo.toEntity())
+    }
+
+    override suspend fun updateTodo(todo: Todo) {
+        todoDao.update(todo.toEntity())
+    }
+
+    override suspend fun moveToTrash(todoId: Long) {
+        val todo = todoDao.getTodoById(todoId) ?: return
+        todoDao.update(todo.copy(isTrashed = true, updatedAt = System.currentTimeMillis()))
+    }
+
+    override suspend fun restoreTodo(todoId: Long) {
+        val todo = todoDao.getTodoById(todoId) ?: return
+        todoDao.update(todo.copy(isTrashed = false, updatedAt = System.currentTimeMillis()))
+    }
+
+    override suspend fun deleteTodoPermanently(todoId: Long) {
+        todoDao.deleteById(todoId)
+    }
+}
