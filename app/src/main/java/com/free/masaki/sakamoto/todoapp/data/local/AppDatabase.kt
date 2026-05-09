8723e16de7477ccdfd9bce2939faa05f17394b6d
坂本将紀 <sbl.sakamoto.masaki@gmail.com>
2026-05-09 14:51:05 +0900

    Databaseクラスの追加。

---
+++ app/src/main/java/com/free/masaki/sakamoto/todoapp/data/local/AppDatabase.kt
@@ -0,0 +1,13 @@
+package com.free.masaki.sakamoto.todoapp.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [TodoEntity::class],
+    version = 1,
+    exportSchema = false,
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun todoDao(): TodoDao
+}
