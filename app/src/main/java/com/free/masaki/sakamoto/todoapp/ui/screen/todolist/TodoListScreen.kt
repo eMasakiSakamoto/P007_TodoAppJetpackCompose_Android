205acebe666dee9912fc0aba05b6b68659331533
坂本将紀 <sbl.sakamoto.masaki@gmail.com>
2026-05-10 16:17:47 +0900

    タスク追加ボタンをわかりやすい表示に変更。

--- app/src/main/java/com/free/masaki/sakamoto/todoapp/ui/screen/todolist/TodoListScreen.kt
+++ app/src/main/java/com/free/masaki/sakamoto/todoapp/ui/screen/todolist/TodoListScreen.kt
@@ -14,9 +14,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -65,7 +68,7 @@ fun TodoListScreen(
         },
         floatingActionButton = {
             FloatingActionButton(onClick = onAddClick) {
-                Text("+")
+                Icon(Icons.Filled.Add, contentDescription = "追加")
             }
         }
     ) { padding ->
